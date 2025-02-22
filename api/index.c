27d55a97692a2b221ad4418ee819ea890580dfe9
marcosfrancomarinho <marcosmarinho19998@gmail.com>
2025-02-22 11:44:43 -0300

    refatorado codigo da rota authenticate

--- api/index.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-   char name[22] = "marcos franco marinho\n";
-   printf("%s", name);
-   return 0;
-}
--- src/interfaces/AuthenticateRouterMiddlewaresTypes.php
+++ src/interfaces/AuthenticateRouterMiddlewaresTypes.php
@@ -2,7 +2,6 @@
 namespace App\interfaces;
 
 use Psr\Http\Server\RequestHandlerInterface;
-use Slim\Handlers\Strategies\RequestHandler;
 use Slim\Psr7\Request;
 
 interface AuthenticateRouterMiddlewaresTypes{
